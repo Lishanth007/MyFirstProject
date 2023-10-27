a8b97105da32a0f674fff7df1381502a08436b6a
Lishanth <120487924+Lishanth007@users.noreply.github.com>
2023-10-27 15:56:16 +0530

    Update Calc.java

--- src/com/edubridge/app1/Calc.java
+++ src/com/edubridge/app1/Calc.java
@@ -4,7 +4,9 @@ public class Calc {
 	public static void findSquare(int num) {
 		System.out.println(num * num);
 	}
-
+	public static void findProduct(int num1, int num2) {
+		System.out.println(num1*num2);
+	}
 	public static void findSum(int num1, int num2) {
 		System.out.println(num1+num2);
 	}
